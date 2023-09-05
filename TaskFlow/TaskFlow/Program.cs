10d31dfbf6fd49cdff7bc1ce5dd29530b832fa0e
gabrieloliveirasouza21 <gabrielosouza21@gmail.com>
2023-09-05 18:24:38 -0300

    Writing and Reading a file added.

--- TaskFlow/TaskFlow/Program.cs
+++ TaskFlow/TaskFlow/Program.cs
@@ -1,7 +1,9 @@
 ﻿int escolha;
 bool running = true;
 int codigo = 0;
-Dictionary<int,string> tasks = new Dictionary<int,string>();
+Dictionary<int, string> tasks = new Dictionary<int, string>();
+string filePath = "C:\\Users\\gabri\\source\\repos\\TaskFlow\\taskflow.txt";
+
 void BoasVindas(String palavra) {
     int qtd_letras = palavra.Length;
     String caractere = string.Empty.PadLeft(qtd_letras, '*');
@@ -35,10 +37,29 @@ void CriarTarefa() {
     Console.WriteLine("\n");
     Console.WriteLine("Forneça uma descrição da tarefa :");
     string descricaoTask = Console.ReadLine()!;
-    
+
+
 
     Tarefa tarefa = new Tarefa(tituloTask, descricaoTask, "em andamento");
     tasks.Add(codigo++, tarefa.toString());
+
+
+    try {
+        if (File.Exists(filePath)) {
+            StreamWriter escritor = new StreamWriter(filePath, true);
+            escritor.WriteLine(tarefa.toString());
+            escritor.Close();
+        } else {
+            StreamWriter escritor = new StreamWriter(filePath);
+            escritor.WriteLine(tarefa.toString());
+            escritor.Close();
+        }
+
+    } catch (Exception e) {
+        Console.WriteLine(e.Message);
+    }
+
+
     Console.WriteLine("\n");
 
     Console.WriteLine("Tarefa adicionada com sucesso !!");
@@ -53,14 +74,29 @@ void LerTarefas() {
     Console.Clear();
     BoasVindas("Visualizando todas as tarefas");
     Console.WriteLine("\n");
-    
-    foreach(var tarefa in tasks) {
+
+    /*foreach(var tarefa in tasks) {
         Console.WriteLine($"{tarefa}");
+    } */
+
+    try {
+        if (File.Exists(filePath)) {
+            StreamReader leitor = new StreamReader(filePath);
+            string linha;
+            while ((linha = leitor.ReadLine()) != null) {
+                Console.WriteLine(linha);
+            }
+            leitor.Close();
+        } else {
+            Console.WriteLine("Arquivo Inexistente, verifique e tente novamente !");
+        }
+    } catch (Exception e) {
+        Console.WriteLine(e.Message);
     }
 
     Console.WriteLine("\n");
     Console.WriteLine("Digite qualquer tecla para voltar ao menu principal");
-    Console.ReadKey(); 
+    Console.ReadKey();
     Menu();
 }
 
@@ -109,7 +145,7 @@ void RemoverTarefas() {
     Console.WriteLine("Digite o código da tarefa que deseja remover : ");
     int codigoTarefa = int.Parse(Console.ReadLine()!);
 
-    
+
 
     if (tasks.ContainsKey(codigoTarefa)) {
         tasks.Remove(codigoTarefa);
@@ -126,16 +162,18 @@ void RemoverTarefas() {
 
 }
 
+
+
 BoasVindas("Seja Bem Vindo ao TaskFlow !!!");
 while (running) {
     Menu();
     escolha = int.Parse(Console.ReadLine()!);
     switch (escolha) {
-        case 1: CriarTarefa();break;
+        case 1: CriarTarefa(); break;
         case 2: LerTarefas(); break;
         case 3: EditarTarefas(); break;
-        case 4: RemoverTarefas() ; break;
-        case 0: running= false; break;
+        case 4: RemoverTarefas(); break;
+        case 0: running = false; break;
     }
 }
 
